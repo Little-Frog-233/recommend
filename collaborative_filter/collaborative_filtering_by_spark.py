edaeff13aee4e962fef1c6b0999809f21dcb6f1f
ruicheng <1342468180@qq.com>
2018-08-15 17:54:57 +0800

    update

--- collaborative_filter/collaborative_filtering_by_spark.py
+++ collaborative_filter/collaborative_filtering_by_spark.py
@@ -119,5 +119,5 @@ class cf_recommend:
 
 if __name__ == '__main__':
 	test_rank([5,10,15,20,25])
-	# cf_recommend = cf_recommend(5)
-	# cf_recommend.run()
+	cf_recommend = cf_recommend(5)
+	cf_recommend.run()
