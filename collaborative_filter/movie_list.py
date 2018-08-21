f8cc1386f67e8f64c0f8e030a7b915dad7bc3763
ruicheng <1342468180@qq.com>
2018-08-21 13:27:29 +0800

    update

--- collaborative_filter/movie_list.py
+++ collaborative_filter/movie_list.py
@@ -4,4 +4,4 @@ from douban_mysql import douban_mysql
 if __name__ == '__main__':
 	factory = douban_mysql()
 	# factory.add_movie_list_all()
-	factory.get_movie_list('/Users/ruicheng/PycharmProjects/Collaborative_filter/collaborative_filter/list.txt',user_max=300)
+	factory.get_movie_list('/Users/ruicheng/PycharmProjects/Collaborative_filter/collaborative_filter/list.txt',user_max=300,get_movie=500)
